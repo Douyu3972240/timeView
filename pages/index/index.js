c48d4763abdb50b3ca809dcce8e1970a828b7175
韩旭 <1301326083@qq.com>
2021-07-04 18:52:44 +0800

    1.更新了首页的UI

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,10 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    changeSwiper: 0, //当前swiper的下标
     showSentenceIndex: 0, //决定显示sentence里面的第几个
     sentence: ['少壮不努力,老大徒伤悲', '花有重开时,人无再少年', '一寸光阴一寸金,寸金难买寸光阴', '等时间的人,就是浪费时间的人', '钉子是敲进去的,时间是挤出来的', '黑发不知勤学早,白首方悔读书迟'],
+    scaleArr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], //用于显示进度条上的小刻度
     animationSentence: [], //用于将句子以动画的方式显示出来
-    scaleArr: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     percent: 0, //百分比，默认为0
     timer: {
       refreshTime: null, //用于控制刷新界面上的时间
@@ -32,10 +33,16 @@ Page({
       second: 0
     }
   },
+  changeSwiper(e) {
+    console.log(e)
+    this.setData({
+      changeSwiper: e.detail.current
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     console.log("index onLoad")
     let _this = this
     let percent = app.time.calculatePercent(`${app.time.getYMDHMS().year}/01/01 00:00:00`, `${app.time.getYMDHMS().year}/12/30 23:59:59`, `${app.time.getYMDHMS().year}/${app.time.getYMDHMS().month}/${app.time.getYMDHMS().day} ${app.time.getYMDHMS().hour}:${app.time.getYMDHMS().minute}:${app.time.getYMDHMS().second}`)
@@ -76,7 +83,7 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
   coolText(text, duration) {
@@ -115,7 +122,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
     console.log("index onShow")
     let _this = this
     clearInterval(_this.data.timer.refreshTime)
@@ -124,9 +131,9 @@ Page({
     _this.data.timer.refreshSentence = setInterval(() => {
       //每5000ms更新一次
       _this.data.showSentenceIndex++
-        if (_this.data.showSentenceIndex == _this.data.sentence.length - 1) {
-          _this.data.showSentenceIndex = 0
-        }
+      if (_this.data.showSentenceIndex == _this.data.sentence.length - 1) {
+        _this.data.showSentenceIndex = 0
+      }
       _this.setData({
         showSentenceIndex: _this.data.showSentenceIndex
       })
@@ -157,7 +164,7 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
     let _this = this
     console.log("onHide")
     clearInterval(_this.data.timer.animationWord)
@@ -168,7 +175,7 @@ Page({
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
     let _this = this
     console.log("onUnload")
     clearInterval(_this.data.timer.animationWord)
@@ -179,29 +186,29 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   },
-  onShareTimeline(){
+  onShareTimeline() {
     console.log("onShareTimeline被调用")
     return {
-      title:"好料不停",
-      query:"pages/test01/test01",
-      imageUrl:""
+      title: "好料不停",
+      query: "pages/test01/test01",
+      imageUrl: ""
     }
   }
 })
